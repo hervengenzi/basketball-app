cab526e44c9440b30a6d40ec7000b3c7b65b2137
hervengenzi <ngenziherve@octangrp.com>
2020-07-21 17:06:05 +0200

    Implemented tab bar menu

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
 import Home from './src/components/Home';
+import {StyleSheet, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 class App extends Component{
   render(){
     return(
-      <Home />
+      <NavigationContainer>
+      <Tab.Navigator style={styles.tab}>
+        <Tab.Screen name="Teams" component={Home} />
+        <Tab.Screen name="Players" component={Home} />
+      </Tab.Navigator>
+    </NavigationContainer>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  tab: {
+    fontSize: 24
+  }
+});
 
 export default App;
