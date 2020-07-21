9a9ebc0d6a0f46a6bb732734773e5057a7d6db3b
hervengenzi <ngenziherve@octangrp.com>
2020-07-21 15:13:37 +0200

    Create home page

---
+++ App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class App extends Component{
+  render(){
+    return(
+      <View>
+        <Text>Here</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1
+},
+});
+export default App;
