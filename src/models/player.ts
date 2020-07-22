773cea8cef7d6da3107a0444f585f7a66768a93f
hervengenzi <ngenziherve@octangrp.com>
2020-07-22 08:38:01 +0200

    Create player model

---
+++ src/models/player.ts
@@ -0,0 +1,15 @@
+class Player{
+    id: number
+    name: string
+    team: string
+    salary: string
+    careerPoints: number
+
+    constructor(id: number, name: string, team: string, salary: string, careerPoints: number){
+        this.id = id,
+        this.name = name,
+        this.team = team,
+        this.salary = salary,
+        this.careerPoints = careerPoints
+    }
+}
