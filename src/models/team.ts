0dc97a8df103af61cf45d9b92885e849309058f3
hervengenzi <ngenziherve@octangrp.com>
2020-07-22 08:30:49 +0200

    Load team pictures

--- src/models/team.ts
+++ src/models/team.ts
@@ -3,12 +3,14 @@ class Team {
     city: string
     teamName: string
     conf: string
+    logo: string
 
-    constructor(id: number, city: string, teamName: string, conf: string){
+    constructor(id: number, city: string, teamName: string, conf: string, logo: string){
         this.id = id,
         this.city = city,
         this.teamName = teamName, 
-        this.conf = conf
+        this.conf = conf,
+        this.logo = logo
     }
 }
 
